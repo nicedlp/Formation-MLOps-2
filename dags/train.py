32d68f2f5462668a560fa2f0f2cec84e9740b191
nicedlp <daphne.pertsekos@gmail.com>
2023-09-08 14:55:44 +0000

    train dags

--- dags/train.py
+++ dags/train.py
@@ -7,9 +7,9 @@ from sqlalchemy_utils.types.enriched_datetime.pendulum_date import pendulum
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-from dags.config import TRAIN_DATA_PATH, MODEL_REGISTRY_FOLDER, DATA_FOLDER
+from dags.config import TRAIN_DATA_PATH, MODEL_REGISTRY_FOLDER, DATA_FOLDER, MODEL_BOOSTING_REGISTRY_FOLDER, MODEL_BOOSTING_PATH
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
-from formation_indus_ds_avancee.train_and_predict import train_model_with_io
+from formation_indus_ds_avancee.train_and_predict import train_model_with_io, train_model_boosting_with_io
 
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(weeks=4),
@@ -25,8 +25,30 @@ def train_model():
     def train_model_task(feature_path: str) -> None:
         train_model_with_io(features_path=feature_path, model_registry_folder=MODEL_REGISTRY_FOLDER)
 
+
     feature_path = prepare_features_task()
     train_model_task(feature_path)
 
 
+
+
+@dag(default_args={'owner': 'airflow'}, schedule=timedelta(weeks=4),
+     start_date=pendulum.today('UTC').add(hours=-1))
+def train_model_boost():
+    @task
+    def prepare_features_task() -> str:
+        feature_train_path: str = os.path.join(DATA_FOLDER, 'prepared_features_train.parquet')
+        prepare_features_with_io(data_path=TRAIN_DATA_PATH, features_path=feature_train_path, training_mode=True)
+        return feature_train_path
+
+    @task
+    def train_model_boost_task(feature_path: str) -> None:
+        train_model_boosting_with_io(features_path=feature_path, model_registry_folder=MODEL_BOOSTING_REGISTRY_FOLDER)
+
+    feature_path = prepare_features_task()
+    train_model_boost_task(feature_path)
+
+
+
 train_model_dag = train_model()
+train_model_boost_dag = train_model_boost()
