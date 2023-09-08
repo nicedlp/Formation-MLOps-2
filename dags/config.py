f905ab11a9d19785f980b1e566fc6ef44f353881
nicedlp <daphne.pertsekos@gmail.com>
2023-09-08 14:54:45 +0000

    conf

--- dags/config.py
+++ dags/config.py
@@ -5,6 +5,7 @@ PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, 'data')
 GENERATED_DATA_FOLDER = os.path.join(DATA_FOLDER, "generated_data")
 PREDICTIONS_FOLDER = os.path.join(DATA_FOLDER, 'predictions')
+PREDICTIONS_BOOSTED_FOLDER = os.path.join(DATA_FOLDER, 'predictions',"boosting")
 
 TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 # TODO change, should point to generator
@@ -15,5 +16,9 @@ FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
 MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, '20200616-140746.joblib')  # To change when needed
 
+MODEL_BOOSTING_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models','boosting')
+MODEL_BOOSTING_PATH = os.path.join(MODEL_BOOSTING_REGISTRY_FOLDER, '20230908-131919.joblib')
+
+
 # Add your initials to the monitoring table name
-MONITORING_TABLE_NAME = 'monitoring'
+MONITORING_TABLE_NAME = 'dp_monitoring'
